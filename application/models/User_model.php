785c984daa5383ec7924bcf37edc0553e3e86306
dev1 <dev@octopuscart.com>
2019-03-02 14:57:56 +0530

    changes

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -117,7 +117,7 @@ class User_model extends CI_Model {
             );
             $this->db->insert('system_log', $orderlog);
 
-            $subject = "Welcome to Octopuscart - Your account with www.octopuscart.com has been successfully created!";
+            $subject = "Welcome to Joogls.com - Your account with www.joogls.com has been successfully created!";
             $this->email->subject($subject);
             
             $customerdetails['customer'] = $customer;
